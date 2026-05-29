ef18897742e8ac54b2b0881b9399ae93d40a30fc
Oshada Rashmika <oshadar.rodrigo@gmail.com>
2026-05-29 16:44:55 +0530

    feat: accept operator input string

--- phase_01_control_flow/calculator.py
+++ phase_01_control_flow/calculator.py
@@ -2,3 +2,4 @@ print ("Welcome to the calculator program!")
 
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation you want to perform (+, -, *, /): ")
