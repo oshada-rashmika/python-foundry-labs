d5bb2cabe74cb8daeca43490f5944ad1205a7bb4
Oshada Rashmika <oshadar.rodrigo@gmail.com>
2026-05-29 16:46:20 +0530

    feat: add basic operations

--- phase_01_control_flow/calculator.py
+++ phase_01_control_flow/calculator.py
@@ -3,3 +3,19 @@ print ("Welcome to the calculator program!")
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 operation = input("Enter the operation you want to perform (+, -, *, /): ")
+
+if operation == "+":
+    result = num1 + num2
+    print(f"The result of {num1} + {num2} is: {result}")
+elif operation == "-":
+    result = num1 - num2
+    print(f"The result of {num1} - {num2} is: {result}")
+elif operation == "*":
+    result = num1 * num2
+    print(f"The result of {num1} * {num2} is: {result}")
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+        print(f"The result of {num1} / {num2} is: {result}")
+    else:
+        print("Error: Division by zero is not allowed.")
