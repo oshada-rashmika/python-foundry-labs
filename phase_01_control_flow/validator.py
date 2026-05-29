671f21a05bc9c193b848750b623216fb2a1dd9da
Oshada Rashmika <oshadar.rodrigo@gmail.com>
2026-05-29 16:51:27 +0530

    feat: implement minimum length validation check

--- phase_01_control_flow/validator.py
+++ phase_01_control_flow/validator.py
@@ -3,3 +3,6 @@ print ("Welcome to the Validator program!")
 password = input("Please enter your password: ")
 
 error = []
+
+if len(password) < 8:
+    error.append("Password must be at least 8 characters long.")
