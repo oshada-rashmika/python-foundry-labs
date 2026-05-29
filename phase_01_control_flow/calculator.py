a265f3edaea1cc31c1b90605ffdd5651211a53b2
Oshada Rashmika <oshadar.rodrigo@gmail.com>
2026-05-29 16:44:29 +0530

    feat: accept and cast float inputs

--- phase_01_control_flow/calculator.py
+++ phase_01_control_flow/calculator.py
@@ -1,1 +1,4 @@
-print ("Welcome to the calculator program!");
+print ("Welcome to the calculator program!")
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
