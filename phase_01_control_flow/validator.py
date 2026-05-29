7a9252a1e5e45562c2c2eeb2afc5232598e87d08
Oshada Rashmika <oshadar.rodrigo@gmail.com>
2026-05-29 16:59:25 +0530

    refactor: complete fully functional password validator with clean reporting

--- phase_01_control_flow/validator.py
+++ phase_01_control_flow/validator.py
@@ -8,7 +8,6 @@ if len(password) < 8:
     error.append("Password must be at least 8 characters long.")
 
 has_number = False
-special_characters = "!@#$%^&*()-_=+[]{}|;:',.<>?/"
 
 for char in password:
     if char.isdigit():
@@ -17,8 +16,19 @@ for char in password:
 if not has_number:
     error.append("Password must contain at least one number.")
 
+special_characters = "!@#$%^&*()-_=+[]{}|;:',.<>?/"
+has_special = False
+
 for char in password:
     if char in special_characters:
+        has_special = True
         break
-if not special_characters:
+if not has_special:
     error.append("Password must contain at least one special character.")
+
+if len(error) == 0:
+    print("Password is valid!")
+else:
+    print("Password is invalid:")
+    for err in error:
+        print(f"- {err}")
